f99dcd02de3b744884062e3aa030c7320b3f9bd0
CaiBytes <cairahbytes@gmail.com>
2025-02-19 19:13:35 -0500

    inital unity upload

---
+++ Assets/kitchen_button.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class kitchen_button : MonoBehaviour
+{
+   
+    public void kitchen_view()
+    {
+        SceneManager.LoadScene("kitchen_view");
+    }
+
+
+
+}
